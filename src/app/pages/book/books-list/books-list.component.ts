4247a1a1457d83e6344119780a911e02ca899d21
Mew <>
2023-01-08 09:43:22 +0700

    del

--- src/app/pages/book/books-list/books-list.component.ts
+++ src/app/pages/book/books-list/books-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CrudService } from 'src/app/service/crud.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-books-list',
@@ -19,12 +20,32 @@ export class BooksListComponent implements OnInit {
   }
 
   delete(id: any, i: any) {
-    console.log(id);
-    if (window.confirm('Are you sure?')) {
-      this.crudService.DeleteBook(id).subscribe((res) => {
-        this.Books.splice(i, 1);
-      });
-    }
+    // console.log(id);
+    // if (window.confirm('Are you sure?')) {
+    //   this.crudService.DeleteBook(id).subscribe((res) => {
+    //     this.Books.splice(i, 1);
+    //   });
+    // }
+    Swal.fire({
+      title: 'คุณแน่ใจไหม?',
+      text: 'ลบแล้วไม่สามารถย้อนกลับได้',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'ใช่ ลบเลย',
+      cancelButtonText: 'ไม่ ขอคิดดูก่อน',
+    }).then((result) => {
+      if (result.value) {
+        this.crudService.DeleteBook(id).subscribe((res) => {
+          this.Books.splice(i, 1);
+        });
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'Your work has been saved',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      }
+    });
   }
-
 }
